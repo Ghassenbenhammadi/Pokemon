12a18a770d699b8d73f64b2d59664e0c20920b20
Ghassenbenhammadi <benhammdighassen@gmail.com>
2023-09-06 18:21:00 +0200

    les modules & les services

---
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,24 @@
+import { PokemonService } from './../pokemon.service';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+
+})
+export class ListPokemonComponent implements OnInit  {
+
+  pokemons: Pokemon[];
+  constructor(
+    private router:Router,
+    private pokemonService:PokemonService){}
+  ngOnInit(){
+    this.pokemons = this.pokemonService.getPokemons();
+  }
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id])
+  }
+}
