83b630e9df8df8a575b584ec790a61420c3c3426
Ghassenbenhammadi <benhammdighassen@gmail.com>
2023-09-06 12:51:31 +0200

    les pipes & les routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
   
 })
-export class AppComponent implements OnInit {
-  pokemons: Pokemon[] =  POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-constructor(){
-
-}
-ngOnInit(): void {
-  console.table(this.pokemons);
-  
-}
-selectPokemon(pokemon: Pokemon) {
-//  const pokemon : Pokemon|undefined = this.pokemons.find(pokemon => pokemon.id == +pokemonId);
- // ajouter +pour transformation string to number
- if(pokemon){
-  console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-  this.pokemonSelected = pokemon;
- } else {
-  console.log('Vous avez demandé un pokémon qui n\existe pas');
-  this.pokemonSelected = pokemon;
- }
-}
-}
+export class AppComponent  {}
